ce50a260de75a43ae2b9ad67f183087389177066
Konstantin Stepanov <me@kstep.me>
2014-11-08 23:15:03 +0300

    output html

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -7,26 +7,36 @@ extern crate url;
 use scgi::{SCGIEnv, SCGIBind, TcpSCGIServer, UnixSCGIServer};
 use std::io::IoResult;
 
-fn process(writer: &mut Writer, env: &SCGIEnv) -> IoResult<()> {
-    try!(writer.write_str("Status: 200 OK\r\n"));
-    try!(writer.write_str("Content-Type: text/plain\r\n"));
-    try!(writer.write_str("\r\n"));
-    try!(writer.write_str("Headers:\n"));
-    for (k, v) in env.env.iter() {
-        try!(writer.write_str(k[]));
-        try!(writer.write_str(" = "));
-        try!(writer.write_str(v[]));
-        try!(writer.write_str("\n"));
-    }
+fn process(w: &mut Writer, env: &SCGIEnv) -> IoResult<()> {
+    try!(w.write_str("Status: 200 OK\r\n"));
+    try!(w.write_str("Content-Type: text/html\r\n"));
+    try!(w.write_str("\r\n"));
 
-    try!(writer.write_str("\n"));
-    try!(writer.write_str("Query string:\n"));
-    for (k, v) in env.query().unwrap().iter() {
-        try!(writer.write_str(k[]));
-        try!(writer.write_str(" = "));
-        try!(writer.write_str(v[]));
-        try!(writer.write_str("\n"));
+    try!(w.write_str("<!DOCTYPE html5><html><body>\n"));
+    try!(w.write_str("<h1>Headers</h1>\n"));
+    try!(w.write_str("<table><thead><tr><th>Name</th><th>Value</th></tr></thead><tbody>"));
+    for (k, v) in env.env.iter() {
+        try!(w.write_str("<tr><td>"));
+        try!(w.write_str(k[]));
+        try!(w.write_str("</td><td>"));
+        try!(w.write_str(v[]));
+        try!(w.write_str("</td></tr>"));
     }
+    try!(w.write_str("</tbody></table>"));
+
+    try!(w.write_str("<h1>Query string</h1>"));
+    try!(w.write_str("<table><thead><tr><th>Name</th><th>Value</th></tr></thead><tbody>"));
+    for (k, v) in env.query().unwrap().iter() {
+        try!(w.write_str("<tr><td>"));
+        try!(w.write_str(k[]));
+        try!(w.write_str("</td><td>"));
+        try!(w.write_str(v[]));
+        try!(w.write_str("</td></tr>"));
+    }
+    try!(w.write_str("</tbody></table>"));
+
+    try!(w.write_str("</pre>"));
+    try!(w.write_str("</body></html>"));
 
     Ok(())
 }
